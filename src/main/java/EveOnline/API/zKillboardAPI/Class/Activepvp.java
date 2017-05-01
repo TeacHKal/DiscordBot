0c950626f560fc0de6823669a77cb48604e6fad4
TeacH Kal <teach.inbox@gmail.com>
2017-05-01 02:43:27 +0200

    GSON parsing fix, array to object - When there is no data for an object it replace object brackets "{}" to array brackets "[]" aka empty array - Implemented method to fix that. Replace array "[]" with object "{}"

--- src/main/java/EveOnline/API/zKillboardAPI/Class/Activepvp.java
+++ src/main/java/EveOnline/API/zKillboardAPI/Class/Activepvp.java
@@ -2,26 +2,25 @@ package EveOnline.API.zKillboardAPI.Class;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
 /**
  * Created by teach on 4/30/2017.
  * email: teach.inbox@gmail.com
  */
-public class Activepvp {
+public class Activepvp implements Serializable{
 
     @SerializedName("systems")
-    @Expose
+    //@Expose
     private Systems systems;
     @SerializedName("kills")
-    @Expose
+    //@Expose
     private Kills kills;
 
-    public Systems getSystems() {
-        return systems;
-    }
+    public Systems getSystems() { return systems; }
 
-    public void setSystems(Systems systems) {
-        this.systems = systems;
-    }
+    public void setSystems(Systems systems) { this.systems = systems; }
 
     public Kills getKills() {
         return kills;
--- src/main/java/EveOnline/API/zKillboardAPI/Service/EntityService.java
+++ src/main/java/EveOnline/API/zKillboardAPI/Service/EntityService.java
@@ -13,6 +13,8 @@ public class EntityService {
 
     private final static String URL_PATH = "https://zkillboard.com/api/stats/";
     private final static String SLASH = "/";
+    private final static String JSON_ARRAY_BRACKETS = "[]";
+    private final static String JSON_OBJECT_BRACKETS = "{}";
 
 
     private static String getDataFromUrl(String entityID, String entityType){
@@ -25,14 +27,26 @@ public class EntityService {
     private static Entity getPlayerObjectFromString(String jsonString) {
         Gson gson = new Gson();
         Entity entity = null;
-
-            entity = gson.fromJson(jsonString, Entity.class);
+            try {
+                entity = gson.fromJson(jsonString, Entity.class);
+            }catch (Exception e){
+                System.out.println("ERROR: " + e.toString());
+            }
 
         return entity;
     }
 
     public static Entity getPlayer(String entityID){
         String dataString = getDataFromUrl(entityID, Entity.PLAYER);
+
+        // When there is no data for an object it replace object brackets "{}" to array brackets "[]"
+        // So the method above fix that. Replace array "[]" with object "{}"
+        dataString = stringReplace(dataString, JSON_ARRAY_BRACKETS, JSON_OBJECT_BRACKETS);
+
         return getPlayerObjectFromString(dataString);
+    }
+
+    private static String stringReplace(String string, String oldPart, String newPart){
+        return string.replace(oldPart, newPart);
     }
 }
